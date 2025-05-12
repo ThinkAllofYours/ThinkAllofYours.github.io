8bd36edd800ba98da0e6a4441a3f29162fd45687
duhwan <e.where.people.enjoy@gmail.com>
2025-05-12 11:55:33 +0900

    Add - Admonition 컴포넌트 ::: 사용 가능

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,6 +43,9 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          mdx: {
+            admonitions: true,
+          },
         },
         blog: {
           showReadingTime: true,
